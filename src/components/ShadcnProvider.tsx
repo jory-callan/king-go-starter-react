2a7757f7a3ce6ce3af08c48eefc7976cc356c5ed
czw <2979049375@qq.com>
2026-01-29 16:53:02 +0800

    feat: add toast

--- src/components/ShadcnProvider.tsx
+++ src/components/ShadcnProvider.tsx
@@ -1,10 +1,12 @@
 import React from "react"
 import { ThemeProvider } from "./theme-provider"
+import { Toaster } from "@/components/ui/sonner"
 
 export function ShadcnProvider({ children }: { children: React.ReactNode }) {
   return (
     <>
       <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+        <Toaster position="top-center" />
         {children}
       </ThemeProvider>
     </>
